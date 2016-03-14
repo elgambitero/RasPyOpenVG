75ee7e88f296ed47d037402688513783937e610b
Jaime Garcia <elgambitero@gmail.com>
2016-03-14 00:48:25 +0000

    Make the rectangles bigger so demo can be seen better

--- polytest/bindtest.py
+++ polytest/bindtest.py
@@ -2,15 +2,16 @@ import display as d
 import time
 
 #Harcoded layers
-layer=(2,((4,1,[400.0,360.0,360.0,400.0],[400.0,400.0,360.0,360.0]),(4,0,[370.0,370.0,390.0,390.0],[370.0,390.0,390.0,370.0])))
+layer=(2,((4,1,[800.0,200.0,200.0,800.0],[800.0,800.0,200.0,200.0]),(4,0,[400.0,400.0,600.0,600.0],[400.0,600.0,600.0,400.0])))
 layer2=(1,((4,1,[800.0,200.0,200.0,800.0],[600.0,600.0,200.0,200.0])))
 
 #init the display and show the layer for 2 seconds.
 d.init()
-d.blank()
-time.sleep(1)
-d.expose(layer)
-time.sleep(2)
-d.blank()
-time.sleep(1)
+for i in range(0,3):
+	d.blank()
+	time.sleep(1)
+	d.expose(layer)
+	time.sleep(2)
+	d.blank()
+	time.sleep(1)
 d.finish()
